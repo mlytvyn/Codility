8744727671169883cbca5a11a65392c026f573ae
Michael Lytvyn <lytvyn.michael@gmail.com>
2014-07-05 23:05:57 +0300

    Lesson 2 - MissingInteger |  33 of 100

---
+++ src/main/java/com/mlytvyn/codility/lesson02/MissingInteger.java
@@ -0,0 +1,50 @@
+package com.mlytvyn.codility.lesson02;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by mlytvyn on 7/5/14.
+ * <p/>
+ * Write a function:
+ * class Solution { public int solution(int[] A); }
+ * that, given a non-empty zero-indexed array A of N integers, returns the minimal positive integer that does not occur in A.
+ * For example, given:
+ * A[0] = 1
+ * A[1] = 3
+ * A[2] = 6
+ * A[3] = 4
+ * A[4] = 1
+ * A[5] = 2
+ * the function should return 5.
+ * Assume that:
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+ * <p/>
+ * int[] A = {1, 3, 6, 1, 4, 2, 7, 9, 3};
+ */
+public class MissingInteger {
+
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int min = 1;
+        Set<Integer> mins = new HashSet<Integer>();
+        Set<Integer> all = new HashSet<Integer>();
+
+        mins.add(min);
+        for (int a : A) {
+            all.add(a);
+            if (a > min) {
+                min = a - 1;
+                mins.add(min);
+            }
+            if (a == min) {
+                min++;
+                mins.add(min);
+            }
+        }
+        mins.removeAll(all);
+        return mins.iterator().next();
+    }
+
+}
---
+++ src/test/java/com/mlytvyn/codility/lesson02/MissingIntegerTest.java
@@ -0,0 +1,67 @@
+package com.mlytvyn.codility.lesson02;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by mlytvyn on 7/5/14.
+ */
+public class MissingIntegerTest {
+
+    private MissingInteger testClass;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        testClass = new MissingInteger();
+    }
+
+    @Test
+    public void testSolution() throws Exception {
+        int[] A = {1, 3, 6, 4, 1, 2};
+        Assert.assertEquals(testClass.solution(A), 5);
+    }
+
+    @Test
+    public void testTwoMissingInts() throws Exception {
+        int[] A = {1, 3, 6, 1, 4, 2, 7, 9, 3};
+        // 5, 8
+        Assert.assertEquals(testClass.solution(A), 5);
+    }
+
+    @Test
+    public void testOneElement() throws Exception {
+        int[] A = {2};
+        Assert.assertEquals(testClass.solution(A), 1);
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        int[] A = {1};
+        Assert.assertEquals(testClass.solution(A), 2);
+    }
+
+    @Test
+    public void testSimple() throws Exception {
+        int[] A = {4, 5, 6, 2};
+        Assert.assertEquals(testClass.solution(A), 1);
+    }
+
+    @Test
+    public void testExtremeMaxMinInt() throws Exception {
+        int[] A = {Integer.MIN_VALUE, Integer.MAX_VALUE};
+        Assert.assertEquals(testClass.solution(A), 1);
+    }
+
+    @Test
+    public void testPositiveOnly() throws Exception {
+        int[] A = new int[200];
+        for (int i = 0; i <= 100; i++) {
+            A[i] = i;
+        }
+        for (int i = 102; i < 200; i++) {
+            A[i] = i;
+        }
+        Assert.assertEquals(testClass.solution(A), 101);
+    }
+}
