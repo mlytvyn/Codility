d028116764468f679f17d2b43f674d83d62f0615
Michael Lytvyn <lytvyn.michael@gmail.com>
2014-07-05 11:28:00 +0300

    Lesson 2 - MaxCounters | 70 of 100 | Replaced for with foreach

---
+++ src/main/java/com/mlytvyn/codility/lesson02/MaxCounters.java
@@ -0,0 +1,54 @@
+package com.mlytvyn.codility.lesson02;
+
+/**
+ * Created by mlytvyn on 7/5/14.
+ * <p/>
+ * Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum.
+ * <p/>
+ * You are given N counters, initially set to 0, and you have two possible operations on them:
+ * increase(X) − counter X is increased by 1,
+ * max counter − all counters are set to the maximum value of any counter.
+ * A non-empty zero-indexed array A of M integers is given. This array represents consecutive operations:
+ * if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+ * if A[K] = N + 1 then operation K is max counter.
+ * For example, given integer N = 5 and array A such that:
+ * A[0] = 3
+ * A[1] = 4
+ * A[2] = 4
+ * A[3] = 6
+ * A[4] = 1
+ * A[5] = 4
+ * A[6] = 4
+ * the values of the counters after each consecutive operation will be:
+ * (0, 0, 1, 0, 0)
+ * (0, 0, 1, 1, 0)
+ * (0, 0, 1, 2, 0)
+ * (2, 2, 2, 2, 2)
+ * (3, 2, 2, 2, 2)
+ * (3, 2, 2, 3, 2)
+ * (3, 2, 2, 4, 2)
+ * The goal is to calculate the value of every counter after all operations.
+ */
+public class MaxCounters {
+
+    public int[] solution(int N, int[] A) {
+        // write your code in Java SE 8
+        int[] counters = new int[N];
+        int maxCounter = 0;
+        for (int k : A) {
+            if (k >= 1 && k <= N) {
+                int x = k - 1;
+                counters[x]++;
+                if (maxCounter < counters[x]) {
+                    maxCounter = counters[x];
+                }
+            }
+            if (k == N + 1) {
+                for (int i = 0; i < counters.length; i++) {
+                    counters[i] = maxCounter;
+                }
+            }
+        }
+        return counters;
+    }
+}
---
+++ src/test/java/com/mlytvyn/codility/lesson02/MaxCountersTest.java
@@ -0,0 +1,31 @@
+package com.mlytvyn.codility.lesson02;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by mlytvyn on 7/5/14.
+ */
+public class MaxCountersTest {
+
+    private MaxCounters testClass;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        testClass = new MaxCounters();
+    }
+
+    @Test
+    public void testSolution() throws Exception {
+        int N = 5;
+        int[] A = {3, 4, 4, 6, 1, 4, 4};
+        int[] expected = {3, 2, 2, 4, 2};
+        int[] actual = testClass.solution(N, A);
+        System.out.println(Arrays.toString(actual));
+        System.out.println(Arrays.toString(expected));
+        Assert.assertEquals(actual, expected);
+    }
+}
